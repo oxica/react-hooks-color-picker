880665dd4ead920977d974dc156d91fdc8e5edc0
Oksana <kskaplun@gmail.com>
2022-05-26 12:48:33 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import ColorPicker from './components/ColorPicker/ColorPicker';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+function App() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 25,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      <ColorPicker options={colorPickerOptions} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import s from './ColorPicker.module.css';
+
+export default function ColorPicker({ options }) {
+  const [activeOptionIdx, setActiveOptionIdx] = useState(0);
+
+  const makeOptionClassName = index => {
+    return index === activeOptionIdx ? s.active : s.option;
+  };
+
+  const { label } = options[activeOptionIdx];
+
+  return (
+    <div className="container">
+      <h2 className="title">Color Picker</h2>
+      <p>Color: {label}</p>
+      <div>
+        {options.map(({ label, color }, index) => (
+          <button
+            key={label}
+            className={makeOptionClassName(index)}
+            style={{ backgroundColor: color }}
+            onClick={() => setActiveOptionIdx(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
